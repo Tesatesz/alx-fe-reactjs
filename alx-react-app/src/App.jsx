a62a2031eb35af6532106e99732da6dd260ec75b
mukumuku Theresa <mukumukutheresa@gmail.com>
2025-08-31 21:31:00 +0100

    Add Header, MainContent, and Footer imports in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,10 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+
+<UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
 
 function App() {
   return (
